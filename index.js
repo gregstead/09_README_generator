3efa99d0f2228defeb931469f651ab9475bf7e63
Greg Stead <gregstead@gmail.com>
2020-11-11 00:04:30 -0600

    working inquirer prompt

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
+const inquirer = require('inquirer');
+
 // array of questions for user
 const questions = [
+    {
+        name: 'github_username',
+        message: 'What is yout gitHub username?',
+        default: 'gregstead',
 
+    },
+    // {
+    //     name: '',
+    //     message: '',
+    //     default: '',
+
+    // }
 ];
 
 // function to write README file
@@ -9,6 +22,11 @@ function writeToFile(fileName, data) {
 
 // function to initialize program
 function init() {
+    inquirer
+        .prompt(questions)
+        .then(answers => {
+            console.log('answers');
+        })
 
 }
 
