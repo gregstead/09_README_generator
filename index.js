2858fc2cfbfe70bf58a2a01da51960682fb3197e
Greg Stead <gregstead@gmail.com>
2020-11-11 19:37:45 -0600

    Finished questions

--- index.js
+++ index.js
@@ -5,24 +5,22 @@ const inquirer = require('inquirer');
 // array of questions for user
 const questions = [
     {
-        name: 'github_username',
-        message: 'What is yout gitHub username?',
-        default: 'gregstead',
-
-    },
-    {
-        name: 'project_title',
+        name: 'title',
         message: 'What is the project title?',
     },
     {
-        name: 'project_description',
+        name: 'description',
         message: 'Describe the project'
     },
     {
-        name: 'contribution_guidelines',
-        message: 'How can developers contribute to the project?',
+        name: 'installation',
+        message: 'How can users install the project?',
         default: '',
-
+    },
+    {
+        name: 'usage',
+        message: 'Explain how to use the project...',
+        default: '',
     },
     {
         type: 'list',
@@ -33,13 +31,21 @@ const questions = [
             'MIT',
             'other',
         ],
-
     },
     {
-        name: 'test_instructions',
-        message: 'Test instructions',
+        name: 'contribute',
+        message: 'How can developers contribute to the project?',
         default: '',
-
+    },
+    {
+        name: 'tests',
+        message: 'Test instructions...',
+        default: '',
+    },
+    {
+        name: 'questions',
+        message: 'How should users get in contact with questions?',
+        default: '',
     },
 ];
 
@@ -52,7 +58,7 @@ function init() {
     inquirer
         .prompt(questions)
         .then(answers => {
-            console.log('answers');
+            console.log(answers);
         })
 
 }
